5099df12c25b34530c1b6fae6a111eba4069a7a0
Asuka Libessart <sotoka.libessart@gmail.com>
2026-05-19 09:52:38 +0200

    + modified version on nix flake

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
             {
                 packages.default = pkgs.stdenv.mkDerivation {
                     pname = "lapifetch";
-                    version = "1.0.0";
+                    version = "1.2.0";
 
                     src = ./.;
 
