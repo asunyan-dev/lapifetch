8df75f3e015b900d4e152e1fb3ded0a383a8b5ef
Asuka Libessart <sotoka.libessart@gmail.com>
2026-05-21 21:04:35 +0200

    Preparing for 1.2.2

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
             {
                 packages.default = pkgs.stdenv.mkDerivation {
                     pname = "lapifetch";
-                    version = "1.2.1";
+                    version = "1.2.2";
 
                     src = ./.;
 
